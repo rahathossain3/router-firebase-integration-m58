26a23dd7d0ec957a034b0d00f1194808000b6ae2
rahathossain3 <rahathossain210@gmail.com>
2022-04-15 02:04:35 +0600

    added unAuthState Change to parsist the auth

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { useEffect, useState } from "react"
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth'
 import app from "../firebase.init";
 
 const auth = getAuth(app);
@@ -21,9 +21,25 @@ const useFirebase = () => {
             })
     }
 
+    //sign out
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => { });
+    }
+
+
+    // for user state change
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            setUser(user);
+        })
+    }, [])
+
+
     // return [user, setUser];
     return {
         user,
+        handleSignOut,
         singInWithGoogle
     }
 
