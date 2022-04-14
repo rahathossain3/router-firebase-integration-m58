971d90d9166adef1c91171a4a894dc2c501b7935
rahathossain3 <rahathossain210@gmail.com>
2022-04-14 22:13:51 +0600

    google user added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+
+    //destructuring 
+    const { singInWithGoogle } = useFirebase();
+
     return (
         <div>
             <h3>Please Login</h3>
             <div style={{ margin: '20px' }}>
-                <button>Google Sign In</button>
+                <button onClick={singInWithGoogle}>Google Sign In</button>
             </div>
 
             <form>
-                <input type="email" name="" placeholder='Enter Email' id="" />
+                <input type="email" name="" placeholder='Enter Email' />
                 <br />
-                <input type="password" name="" placeholder='Password' id="" />
+                <input type="password" name="" placeholder='Password' />
                 <br />
                 <input type="submit" value="Login" />
             </form>
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,17 +1,31 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import app from "../firebase.init";
+
+const auth = getAuth(app);
+
+// google provider 
+const googleProvider = new GoogleAuthProvider();
+
 
 const useFirebase = () => {
     const [user, setUser] = useState({});
 
-    useEffect(() => {
-
-    })
-
-    const singInwithGoogle = () => {
-        console.log('sign in soon')
+    // for google
+    const singInWithGoogle = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const user = result.user;
+                setUser(user);
+                console.log(user);
+            })
     }
 
-    return [user, setUser];
+    // return [user, setUser];
+    return {
+        user,
+        singInWithGoogle
+    }
 
 }
 
