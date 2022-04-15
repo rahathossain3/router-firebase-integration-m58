b007227dfafb0b976c1f0963a18b9b3abbaf5c60
rahathossain3 <rahathossain210@gmail.com>
2022-04-15 06:08:39 +0600

    Private / protected  Route system add

---
+++ src/components/RequireAuth/RequireAuth.js
@@ -0,0 +1,26 @@
+
+import { getAuth } from 'firebase/auth';
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
+import app from '../../firebase.init';
+
+const auth = getAuth(app);
+
+const RequireAuth = ({ children }) => {
+
+    const [user] = useAuthState(auth);
+
+    const location = useLocation();
+
+    // condition
+    if (!user) {
+
+        return <Navigate to="/login" state={{ from: location }} replace />;
+
+    }
+
+    return children;
+};
+
+export default RequireAuth;
